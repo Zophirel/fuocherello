5d8d8cad55cb50c11edda37f3a7cab8cf662a838
Francesco <sworld.design@gmail.com>
2022-11-18 17:56:21 +0100

    implemented search screen animations

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:fuocherello/carousel.dart';
 import 'searchbar.dart' as src_bar;
@@ -30,11 +32,13 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   double searchListWidth = 0;
   double listCtnHeight = 0;
   double listCtnWidth = 0;
-
+  double searchListCtnHeight = 0;
+  double srcBoxHeight = 0;
   double bgHeight =
       MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height;
 
   late List<Widget> homepageInitComponents, homepageSearchOpen, screenState;
+
   @override
   void initState() {
     super.initState();
@@ -43,22 +47,31 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   void setComponents(bool opened) {
     homepageInitComponents = [
       AnimatedContainer(
-        height: double.infinity,
-        width: double.infinity,
-        alignment: Alignment.bottomCenter,
         duration: const Duration(milliseconds: 300),
-        color: Colors.blue,
-        child: const Padding(
-          padding: EdgeInsets.only(top: 100),
-          child: SearchList(),
+        curve: Curves.easeOutCubic,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: AnimatedContainer(
+          height: searchListCtnHeight,
+          alignment: Alignment.bottomCenter,
+          duration: const Duration(milliseconds: 300),
+          color: Colors.white,
+          curve: Curves.easeInOut,
+          child: const Padding(
+            padding: EdgeInsets.only(top: 100),
+            child: SearchList(),
+          ),
         ),
       ),
       AnimatedContainer(
         duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOutCubic,
-        height: bgHeight,
-        width: MediaQuery.of(context).size.width,
-        color: Theme.of(context).colorScheme.primary,
+        height: srcBoxHeight,
+        color: Colors.amber,
+        curve: Curves.easeInOut,
       ),
       Padding(
         padding: const EdgeInsets.fromLTRB(10, 56, 10, 10),
@@ -106,15 +119,104 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
       ),
     ];
+
     homepageSearchOpen = [
       AnimatedContainer(
-        alignment: Alignment.topCenter,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOutCubic,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: AnimatedContainer(
+          height: searchListCtnHeight,
+          alignment: Alignment.bottomCenter,
+          duration: const Duration(milliseconds: 300),
+          color: Colors.white,
+          child: const Padding(
+            padding: EdgeInsets.only(top: 100),
+            child: SearchList(),
+          ),
+        ),
+      ),
+      AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        height: srcBoxHeight,
+        color: Colors.amber,
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10, 56, 10, 10),
+        child: AnimatedOpacity(
+          curve: Curves.easeOutCubic,
+          duration: const Duration(milliseconds: 100),
+          opacity: carouselOpct,
+          child: const CarouselPage(),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10, 56, 10, 10),
+        child: Stack(
+          alignment: AlignmentDirectional.topCenter,
+          fit: StackFit.loose,
+          children: [
+            SizedBox(
+              height: 56,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: const IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      const src_bar.SearchBar(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    ];
+
+    /* homepageSearchOpen = [
+      AnimatedContainer(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.bottomCenter,
         duration: const Duration(milliseconds: 300),
         color: Colors.blue,
-        child: const Padding(
-          padding: EdgeInsets.only(top: 150),
-          child: SizedBox(width: 300, child: SearchList()),
+        child: AnimatedContainer(
+          duration: const Duration(microseconds: 600),
+          height: MediaQuery.of(context).size.height - 200,
+          alignment: Alignment.topCenter,
+          color: Colors.white,
+          child: const Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: SizedBox(
+              width: 300,
+              child: SearchList(),
+            ),
+          ),
         ),
+      ),
+      Container(
+        color: Colors.amber,
+        height: srcBoxHeight,
       ),
       Padding(
         padding: const EdgeInsets.fromLTRB(10, 56, 10, 10),
@@ -153,6 +255,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
       ),
     ];
+    */
     if (!opened) {
       screenState = homepageInitComponents;
     } else {
@@ -174,6 +277,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               bgHeight = 0;
               listCtnHeight = MediaQuery.of(context).size.height - 1;
               listCtnWidth = MediaQuery.of(context).size.width;
+              srcBoxHeight = 130;
+              searchListCtnHeight = MediaQuery.of(context).size.height - 130;
             },
           );
           Future.delayed(
@@ -193,7 +298,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             },
           );
           Future.delayed(
-            const Duration(milliseconds: 200),
+            const Duration(milliseconds: 300),
             (() {
               setState(() {
                 opened = false;
